5eb21cbf1c914fad5f2e9eea64d2fc97292bfb6d
Shahwez Ahmad <shahwez@Shahwezs-MacBook-Air.local>
2024-04-25 07:11:03 +0530

    leart some hooks(useRouter, useParams, useSearchParams)from next/navigation

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,16 @@
+
+'use client'
+import {useRouter, useParams, useSearchParams} from 'next/navigation'
+const IdPage = () => {
+  const router  = useRouter()
+  const {id} = useParams()
+  const query = useSearchParams()
+  return (
+    <div>
+      <h1>IdPage</h1>
+      <button onClick={()=> router.push('/properties')} className='bg-blue-500' >Navigation hooks {id}, {query.get('age')} </button>
+    </div>
+  )
+}
+
+export default IdPage
