60d20de797d8ef2bea93e0e1c8ce89a99f9475dc
Shahwez Ahmad <shahwez@Shahwezs-MacBook-Air.local>
2024-04-28 08:34:31 +0530

    add properities, conditional rendering, use [id]

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,11 +1,23 @@
+
 import Link from "next/link"
+import properties from '@/properties.json'
+import PropertyCard from "@/components/PropertyCard"
 
 const PropertyPage = () => {
  return (
-    <div>
-        <h1 >Propertypage</h1>
-        <Link href="/">home</Link>
-    </div>
+    <section class="px-4 py-6">
+    <div class="container-xl lg:container m-auto px-4 py-6">
+        { properties.length === 0 ? (<p>No Properties Found</p>) : (
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                    {properties.map((property)=> (
+                        <PropertyCard key={property._id} property = {property} />
+                    ))}
+        </div>
+            )
+        }
+      
+        </div>
+        </section>
  )
 }
 
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     const year = new Date().getFullYear()
     return (
         <>
-        <footer className="bg-gray-200 py-4 mt-auto">
+        <footer className="bg-gray-200 py-4 mt-24">
       <div
         className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4"
       >
