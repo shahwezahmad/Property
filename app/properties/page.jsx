83c889cc1ec39126e620b59295c14be9d2783cff
Shahwez Ahmad <shahwez@Shahwezs-MacBook-Air.local>
2024-04-29 08:02:06 +0530

    add loading, not-found files , add recent Property feature

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -5,10 +5,10 @@ import PropertyCard from "@/components/PropertyCard"
 
 const PropertyPage = () => {
  return (
-    <section class="px-4 py-6">
-    <div class="container-xl lg:container m-auto px-4 py-6">
+    <section className="px-4 py-6">
+    <div className="container-xl lg:container m-auto px-4 py-6">
         { properties.length === 0 ? (<p>No Properties Found</p>) : (
-                <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                     {properties.map((property)=> (
                         <PropertyCard key={property._id} property = {property} />
                     ))}
