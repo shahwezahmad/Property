e0f96c908fff1590286d8f9346cb12020fe11c1f
Shahwez Ahmad <shahwez@Shahwezs-MacBook-Air.local>
2024-05-08 23:49:00 +0530

    added action post to form

--- app/api/properties/route.jsx
+++ app/api/properties/route.jsx
@@ -2,12 +2,20 @@ import connectDB from "@/config/database";
 import Property from "@/models/Property";
 
 export const GET = async (request) => {
-    try{
-        await connectDB()
-            const properties  = await Property.find({})
-            return new Response(JSON.stringify(properties),{status:200})
-    }catch(error){
-        console.log(error);
-        return new Response('Something went wrong',{status:400})
-    }
-}
+  try {
+    await connectDB();
+    const properties = await Property.find({});
+    return new Response(JSON.stringify(properties), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 400 });
+  }
+};
+
+export const POST = (request) => {
+  try {
+    return new Response({ message: "Success" }, { status: 200 });
+  } catch (error) {
+    return new Response("Failed", { status: 500 });
+  }
+};
--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -66,7 +66,7 @@ const PropertyAddForm = () => {
     setFields((prev) => ({ ...prev, images: updateImages }));
   };
   return (
-    <form>
+    <form action="/api/properties" method="POST" encType="multipart/form-data">
       <h2 className="text-3xl text-center font-semibold mb-6">Add Property</h2>
 
       <div className="mb-4">
